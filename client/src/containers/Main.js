3c68b29077040b0226a898cb43d362bbba5764c3
Tyler Negro <TylerNegro@Tylers-MacBook-Pro.local>
2018-06-08 16:16:09 -0600

    Private routes working

--- client/src/containers/Main.js
+++ client/src/containers/Main.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './Home';
+import Private from './Private'; // for testing purposes
 import AuthContainer from './AuthContainer';
+import PrivateRoute from '../components/PrivateRoute';
+
+// ? Check on the private route I am using. Test it thouroughly
 
 class Main extends Component {
   render() {
-    const { updateAuth } = this.props;
+    const { updateAuth, isAuth } = this.props;
     return (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/login" render={props => <AuthContainer {...props} updateAuth={updateAuth} />} />
         <Route path="/signup" render={props => <AuthContainer {...props} updateAuth={updateAuth} />} />
+
+
+        <PrivateRoute exact path="/private" component={Private} isAuth={isAuth} />
       </Switch>
 
     );
